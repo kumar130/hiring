fa141afe960b2abde4823c1c9926e1ba69fe1e90
Saurav <saurav.tech812@gmail.com>
2018-05-17 12:38:12 +0530

    Changes

---
+++ helloworld-springboot/src/test/java/test/Selenium.java
@@ -0,0 +1,85 @@
+package test;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+ 
+
+public class Selenium{
+
+               
+
+                @Test
+
+                public void GoogleSearchTest(){
+
+                               
+
+                                // For windows
+
+                                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/" + "chromedriver.exe");
+
+       
+
+                                //For Linux
+
+                                //System.setProperty("webdriver.chrome.driver", "/home/sousr/Downloads/chromedriver");
+
+                                //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/" + "chromedriver");
+
+                               
+
+                                WebDriver driver = new ChromeDriver();
+
+        try{
+
+                                                driver.manage().window().maximize();
+
+                                                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+                                                String baseUrl = "http://google.com/";
+                                                
+                                                
+
+                                                driver.get(baseUrl);
+                                                
+                                                Assert.assertEquals(driver.getTitle(), "Google");
+
+                                                driver.findElement(By.name("q")).sendKeys("Selenium");
+
+                                                driver.findElement(By.xpath("//li//input[@value='Google Search']"))
+
+                                                                                .click();
+
+        }
+
+        catch(Exception e){
+
+                e.printStackTrace();
+
+        }
+
+        finally{
+
+                driver.close();
+
+                                driver.quit();
+
+        }
+
+                               
+
+ 
+
+                }
+
+ 
+
+}
+
+ 
--- helloworld-springboot/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-delete from entity;
-
-insert into entity(id, name) values(1,'entity1');
-insert into entity(id, name) values(2,'entity2');
-insert into entity(id, name) values(3,'entity3');
